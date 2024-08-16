18bf9a507a9f25a8f72292afb5c38297bf637b5d
Andre <andre_fabriciow@hotmail.com>
2024-08-16 17:36:50 -0300

    Added local health_checker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder,};
+use paperclip::actix:: {OpenApiExt, Apiv2Schema, api_v2_operation};
+use serde::{Serialize, Deserialize};
+
+
+async fn health_checker(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>{
+    HttpServer::new( || {
+        App::new()
+            .route("/health_check", web::get().to(health_checker))
+    })
+        .bind("127.0.0.1:8000")
+        ?.run()
+        .await
 }
