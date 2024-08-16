087b9fbd8642ba6adf181ab00cb2ab57950c2216
Andre <andre_fabriciow@hotmail.com>
2024-08-16 19:07:10 -0300

    Creating first integration test

---
+++ src/databases/postgres_connection/mod.rs
@@ -0,0 +1,8 @@
+use sqlx::{postegres::PgPoolOptions, Pool, Postegres};
+
+
+
+
+pub async fn start_connection() -> Poll<Postegres>{
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder,};
-use paperclip::actix:: {OpenApiExt, Apiv2Schema, api_v2_operation};
-use serde::{Serialize, Deserialize};
-
-
-async fn health_checker(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use Modulite::run;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error>{
-    HttpServer::new( || {
-        App::new()
-            .route("/health_check", web::get().to(health_checker))
-    })
-        .bind("127.0.0.1:8000")
-        ?.run()
-        .await
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
+
