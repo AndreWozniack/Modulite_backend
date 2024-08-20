7e2b5e6ccd1eec0bbad75d49599db64b1206b81c
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2024-08-20 02:13:12 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("0.0.0.0:80").expect("Failed to bind port 80");
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind port 80");
     run(listener)?.await
 }
